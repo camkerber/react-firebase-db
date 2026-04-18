98dd67cf12db2aeb1e43138be896afa80b5bcbe7
camkerber <camjkerber@gmail.com>
2026-04-18 09:48:08 -0600

    feat: react 19 and react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,22 @@
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import { version } from "./package.json";
-import { visualizer } from "rollup-plugin-visualizer";
+import react, {reactCompilerPreset} from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+import {resolve} from "path";
+import {version} from "./package.json";
 import typescript from "@rollup/plugin-typescript";
+import babel from "@rolldown/plugin-babel";
 
 console.log(`ReactFirebaseDB v${version}`);
-
-const externals = [
-  "react",
-  "firebase",
-  "firebase/app",
-  "firebase/auth",
-  "firebase/database",
-  "@firebase/app",
-  "@firebase/auth",
-  "@firebase/database",
-];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     typescript(),
-
-    // Visualize the bundle in `stats.html`.
-    // Helps make sure we aren't pulling in extra deps
-    visualizer({ template: "treemap" }),
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
   define: {
     "process.env.REACT_FIREBASE_DB_VERSION": JSON.stringify(version),
@@ -39,12 +27,5 @@ export default defineConfig({
       name: "ReactFirebaseDB",
       fileName: "index",
     },
-    rollupOptions: {
-      external: externals,
-      output: {
-        globals: { react: "React" },
-      },
-    },
-    sourcemap: true,
   },
 });
